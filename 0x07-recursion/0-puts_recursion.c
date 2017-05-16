98e27d957c2246826c6f188a40611f0381d740b1
johndspence <johndspence@gmail.com>
2017-05-16 22:09:02 +0000

    code check 2

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -1,14 +1,13 @@
 #include "holberton.h"
 
 /**
- * _puts_recursion- prints a string recursively
+ * _puts_recursion - prints a string recursively.
  * @s: string
  *
  * Description: Prints a string recursively followed by new line
- * using write
- * Return: Void
+ * using write.
+ * Return: Void.
  */
-
  void _puts_recursion(char *s)
  {
      if(!*s)

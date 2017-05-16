03c62af20e345af46e76025a14d43e10b97d3055
johndspence <johndspence@gmail.com>
2017-05-16 22:30:23 +0000

    post betty check

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -8,13 +8,13 @@
  * using write.
  * Return: Void.
  */
- void _puts_recursion(char *s)
- {
-     if (!*s)
-     {
-         _putchar('\n');
-         return;
-     }
-     _putchar(*s);
-     _puts_recursion(s + 1);
- }
+void _puts_recursion(char *s)
+{
+	if (!*s)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}

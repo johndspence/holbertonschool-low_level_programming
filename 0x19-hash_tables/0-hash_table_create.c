8b5f1b54f9d21cfb4884d57cc55d912ac7a6f491
johndspence <johndspence@gmail.com>
2017-05-18 03:13:19 +0000

    Corrected hash table create

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -5,7 +5,7 @@
  * @size: number of elements in the has table array.
  *
  * Return: a pointer to the newly created hash table.
- * 
+ *
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -19,12 +19,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_ptr = malloc(sizeof(hash_table_t));
 	if (hash_table_ptr == NULL)
 	{
+		free(hash_table_ptr);
 		return (NULL);
 	}
 	hash_table_ptr->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table_ptr->array == NULL)
 	{
-		free(hash_table_ptr);
+		free(hash_table_ptr->array);
 		return (NULL);
 	}
 	hash_table_ptr->size = size;

32773a4ad5c80de02cb52682dbeaafc6eb528ba3
johndspence <johndspence@gmail.com>
2017-05-18 03:01:05 +0000

    Corrected hash table create

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -15,29 +15,27 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table_ptr;
-	hash_node_t **array;
 	unsigned long int i;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	array = malloc(sizeof(*array) * size);
-	if (array == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < size; i++)
-	{
-		array[i] = NULL;
-	}
 	hash_table_ptr = malloc(sizeof(hash_table_t));
 	if (hash_table_ptr == NULL)
 	{
 		return (NULL);
 	}
-
+	hash_table_ptr->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table_ptr->array == NULL)
+	{
+		free(hash_table_ptr);
+		return (NULL);
+	}
 	hash_table_ptr->size = size;
-	hash_table_ptr->array = array;
+	for (i = 0; i < size; i++)
+	{
+		hash_table_ptr->array[i] = NULL;
+	}
 	return (hash_table_ptr);
 }

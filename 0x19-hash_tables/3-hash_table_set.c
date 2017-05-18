8cc4d6be292a5a2266065b36ab73ad046321ada6
johndspence <johndspence@gmail.com>
2017-05-18 05:44:48 +0000

    formatting corrections

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -15,12 +15,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 
 	if (!ht || !key || !value || strlen(key) == 0)
-	{
 		return (0);
-	}
-
 	index = key_index((unsigned char *)key, ht->size);
-	
 	if (ht->array[index])
 	{
 		temp_node_ptr = ht->array[index];
@@ -37,9 +33,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-	{
-		return(0);
-	}
+		return (0);
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
